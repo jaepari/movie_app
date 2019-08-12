b979f8aeb98136b823b1c63531f4da5ae2b9eeea
김재훈 <jaehoonkim@gimjaehun-ui-MacBookPro.local>
2019-08-12 12:55:37 +0900

    Updates

---
+++ precache-manifest.fb176d372b70d4f72f9d06dcc11b0b7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142b9ac7047d6259e05804b454da3bb6",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "81be4048d7d2824e6c09",
+    "url": "/movie_app_2019/static/css/main.d26d11bb.chunk.css"
+  },
+  {
+    "revision": "15117dc60887805479ef",
+    "url": "/movie_app_2019/static/js/2.342d78b8.chunk.js"
+  },
+  {
+    "revision": "81be4048d7d2824e6c09",
+    "url": "/movie_app_2019/static/js/main.2832a6b2.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/movie_app_2019/precache-manifest.fb176d372b70d4f72f9d06dcc11b0b7b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app_2019/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
